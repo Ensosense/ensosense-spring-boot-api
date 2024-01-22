ae0a97c50cca6f0f308384375f63a5f623fb5b1b
Lilia Bekuzina <lilia.bekuzina1@gmail.com>
2024-01-22 20:34:39 +0100

    Add checkout entity

---
+++ src/main/java/se/artshop/ensosensespringbootapi/Checkouts/Checkout.java
@@ -0,0 +1,38 @@
+package se.artshop.ensosensespringbootapi.Checkouts;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "checkout")
+@Data
+public class Checkout {
+
+  public  Checkout(){}
+
+  public Checkout(String userEmail, String checkoutDate, Long artworkId){
+    this.userEmail = userEmail;
+    this.checkoutDate = checkoutDate;
+    this.artworkId = artworkId;
+  }
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+
+  @Column(name = "user_email")
+  private String userEmail;
+
+
+  @Column(name = "checkout_date")
+  private String checkoutDate;
+
+  @Column(name = "artwork_id")
+  private Long artworkId;
+}
